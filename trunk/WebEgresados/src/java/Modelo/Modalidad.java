c93dbeb33142006bab67f15527b8bddba8217271
yurylcg@gmail.com <yurylcg@gmail.com@9b026c87-7043-1c67-d4ca-66a0aab60a21>
2014-08-16 11:39:28 +0000

    Objetos para educación formal.

---
+++ trunk/WebEgresados/src/java/Modelo/Modalidad.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelo;
+
+/**
+ *
+ * @author YURY
+ */
+public class Modalidad {
+    private long idModalidad;
+    private String nombre;
+
+    /**
+     * @return the idModalidad
+     */
+    public long getIdModalidad() {
+        return idModalidad;
+    }
+
+    /**
+     * @param idModalidad the idModalidad to set
+     */
+    public void setIdModalidad(long idModalidad) {
+        this.idModalidad = idModalidad;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
