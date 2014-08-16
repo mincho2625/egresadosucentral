08761ec03e03eddcabbf1ebed9649ce26a1d93a8
yurylcg <yurylcg@gmail.com>
2014-08-16 11:39:28 +0000

    Objetos para educación formal.

---
+++ WebEgresados/src/java/Modelo/AreaEstudios.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelo;
+
+/**
+ *
+ * @author YURY
+ */
+public class AreaEstudios {
+    private long idAreaEstudios;
+    private String nombre;
+
+    /**
+     * @return the idAreaEstudios
+     */
+    public long getIdAreaEstudios() {
+        return idAreaEstudios;
+    }
+
+    /**
+     * @param idAreaEstudios the idAreaEstudios to set
+     */
+    public void setIdAreaEstudios(long idAreaEstudios) {
+        this.idAreaEstudios = idAreaEstudios;
+    }
+}
