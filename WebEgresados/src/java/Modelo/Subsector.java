b46d68f4eb7ad2a6bb50bc5363e7e1389ed592ea
yurylcg <yurylcg@gmail.com>
2014-08-23 02:45:10 +0000

    Cambio en la implementación del crud para ver los nombres de las listas. Ver Distinciones y reconocimientos.

---
+++ WebEgresados/src/java/Modelo/Subsector.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modelo;
+
+/**
+ *
+ * @author JERONIMO
+ */
+public class Subsector {
+    
+    private Long idSubsector;
+    private String nombre;
+    private boolean estado;
+
+    public Long getIdSubsector() {
+        return idSubsector;
+    }
+
+    public void setIdSubsector(Long idSubsector) {
+        this.idSubsector = idSubsector;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+}
